b0f9cccb60b3a21e7ed3247fc6d7fb2ecadcadfc
Scardell <153379926+scardell@users.noreply.github.com>
2024-03-16 08:54:47 +0100

    primeros arcivos

---
+++ input_calculator.py
@@ -0,0 +1,5 @@
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(first_number + second_number) # treatment as a string
+print(int(first_number) + int(second_number)) # treatment as a integer
---
+++ unit_converter.py
@@ -0,0 +1,11 @@
+# Enter code below
+parsecs = 11
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
+
+
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
