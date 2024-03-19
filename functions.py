4b35d17e550a7c0ba20f3e2e5dcd9aecb02f91c5
Scardell <153379926+scardell@users.noreply.github.com>
2024-03-19 21:32:48 +0100

    functions

---
+++ functions.py
@@ -0,0 +1,30 @@
+# Aspectos basicos de las funciones
+# Funciones sin argumentos
+
+def rocket_parts():
+    print("payload, propellant, structure")
+
+rocket_parts()
+
+output = rocket_parts()
+
+output is None
+
+def rocket_parts():
+    return "payload, propellant, structure"
+output = rocket_parts()
+output
+
+# Argumentos opcionales y requeridos
+
+linea_vacia = " "
+print(linea_vacia)
+
+any([True, False, False])
+any([False, False, False])
+str()
+str(15)
+
+# uso de argumentos de funcion
+
+
