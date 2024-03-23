07e33f815d689f56b88d03c231b46a7b076cecf3
Scardell <153379926+scardell@users.noreply.github.com>
2024-03-23 07:49:49 +0100

    modifiec

--- control_errores/config.py
+++ control_errores/config.py
@@ -15,15 +15,15 @@ if __name__ == '__main__':
 try:
     open("mars.jpg")
 except FileNotFoundError as err:
-     print("Got a problem trying to read the file:", err)
+	print("Got a problem trying to read the file:", err)
 
 
 try:
-    open("config.txt")
+	open("config.txt")
 except OSError as err:
-     if err.errno == 2:
-         print("Couldn't find the config.txt file!")
-     elif err.errno == 13:
+	if err.errno == 2:
+		print("Couldn't find the config.txt file!")
+    elif err.errno == 13:
         print("Found config.txt but couldn't read it")
 
 # Ejericio 1
